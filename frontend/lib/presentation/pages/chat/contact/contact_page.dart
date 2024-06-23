06f24b952dd2ffdc7f807f688292497eb7a2320f
oniqy <vyle.111003@gmail.com>
2024-06-23 11:54:51 +0700

    [REF][UI002] ref UI chat

---
+++ frontend/lib/presentation/pages/chat/contact/contact_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class ContactPage extends StatefulWidget {
+  const ContactPage({super.key});
+
+  @override
+  State<ContactPage> createState() => _ContactPageState();
+}
+
+class _ContactPageState extends State<ContactPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('contact'.tr),
+      ),
+    );
+  }
+}

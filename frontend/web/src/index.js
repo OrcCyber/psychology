6c4a2287706651a47711dc6d1dfca30fe1bd97c0
Bùi Đức Tuân <buiductuan12081995@gmail.com>
2024-06-24 23:27:14 +0700

    [FIX] gitignore

---
+++ frontend/web/src/index.js
@@ -0,0 +1,10 @@
+import ReactDom from "react-dom/client";
+import App from "./app";
+import React from "react";
+
+const root = ReactDom.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
